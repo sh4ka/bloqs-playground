de709e62e75493f87e019abaacb180924e1d288f
Jesús Flores <jesus.flores@bq.com>
2015-03-12 17:02:46 +0100

    Factory code support

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,4 @@
 var sandbox = $('#sandbox');
-var field = SVG('sandbox').size('100%', '100%');
 var bloqs = bloqs();
 
 var bloqArr = [];
@@ -7,37 +6,78 @@ var bloqArr = [];
 var newBloqWidth = 200;
 var newBloqHeight = 100;
 
-var bloq1 = {
-    connections: [{location:'up',type:'int'},{location:'down',type:'int'},{location:'right', type:'int'},{location:'left', type:'int'}],
-    size: [200, 100],
-    color: '#000',
-    code: {setup:"1", loop:"1"}
+var canvas = bloqs.createCanvas('sandbox');
+
+var getRandomColor = function () {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    // letter are white
+    if (color == '#FFFFFF') {
+        return getRandomColor();
+    }
+    return color;
 };
 
-var getRandomColor = function(){
-    return '#'+Math.floor(Math.random()*16777215).toString(16);
+var createSetup = function () {
+    var setup = {
+        label: 'setup',
+        down: true,
+        size: [200, 50],
+        color: '#000',
+        code: {setup: "", loop: "void setup (){\n"}
+    };
+    bloqs.createBloq(setup, canvas, [1, 1]);
 };
 
-var createBloq = function(){
-    var xpos = Math.floor(Math.random(0, 1) * sandbox.width() + (newBloqWidth));
-    var ypos = Math.floor(Math.random(0, 1) * sandbox.height() + (newBloqHeight));
+var createLoop = function () {
+    var loop = {
+        label: 'loop',
+        down: true,
+        size: [200, 50],
+        color: '#000',
+        code: {setup: "", loop: "void loop (){\n"}
+    };
+    bloqs.createBloq(loop, canvas, [400, 1]);
+};
 
-    var fillColor = getRandomColor();
+var createBloq = function () {
+    var label = $('#bloq_label').val() == 'loop' ||
+    $('#bloq_label').val() == 'setup' ||
+    $('#bloq_label').val() == '' ? '' + Math.floor(Math.random() * 1000) : $('#bloq_label').val();
+
+    var codeSetup = $('#bloq_code_setup').val() != '' ? $('#bloq_code_setup').val() : '';
+    var codeLoop = $('#bloq_code_loop').val() != '' ? $('#bloq_code_loop').val() : '';
+
+    var fillColor = $('#bloq_color').val() != '' ? $('#bloq_color').val() : getRandomColor();
 
     var bloqData = {
-        connections: [{location:'up',type:'int'},{location:'down',type:'int'},{location:'right', type:'int'},{location:'left', type:'int'}],
+        up: true,
+        down: true,
+        label: label,
+        inputs: ['int', 'string'],
+        output: 'string',
         size: [newBloqWidth, newBloqHeight],
         color: fillColor,
-        code: {setup:"1", loop:"1"}
+        code: {setup: codeSetup, loop: codeLoop}
     };
-    bloqArr.push(bloqs.createBloq(bloqData, field, [xpos, ypos]));
+    bloqData.factoryCode = JSON.stringify(bloqData);
+    var bloq = bloqs.createBloq(bloqData, canvas, [0, 0]);
+    bloq.on('click', function(){
+        // show factory code
+        $('#factory-code').val(this.factoryCode);
+    });
+    bloqArr.push(bloq);
 };
 
-var removeBloqs = function(){
-    $.each(bloqArr, function(bloq){
+var removeBloqs = function () {
+    $.each(bloqArr, function (bloq) {
         bloqArr[bloq].remove();
     });
 };
 
-bloqArr.push(bloqs.createBloq(bloq1, field, [300,100]));
+createSetup();
+createLoop();
 
